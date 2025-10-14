ffdb58afe6c8b6f86621b7cf6031ab15f86b7541
Optifyne <141728033+Optifyne@users.noreply.github.com>
2025-10-14 13:09:22 +0200

    MythicDungeonsCompatibility
    MythicDungeonsCompatibility

---
+++ bukkit/legacy/src/main/java/io/github/wysohn/triggerreactor/bukkit/tools/SerializableLocation.java
@@ -0,0 +1,43 @@
+package io.github.wysohn.triggerreactor.bukkit.tools;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.configuration.serialization.ConfigurationSerializable;
+import org.bukkit.configuration.serialization.SerializableAs;
+import org.bukkit.util.NumberConversions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SerializableAs(value = "org.bukkit.Location")
+public class SerializableLocation extends Location implements ConfigurationSerializable {
+
+    public SerializableLocation(Location location) {
+        super(location.getWorld(), location.getX(), location.getY(), location.getZ(), location.getYaw(),
+                location.getPitch());
+    }
+
+    @Override
+    public Map<String, Object> serialize() {
+        Map<String, Object> data = new HashMap<String, Object>();
+        World w = this.getWorld();
+        if (w != null) {
+            data.put("world", w.getName());
+        }
+        data.put("x", this.getX());
+        data.put("y", this.getY());
+        data.put("z", this.getZ());
+        data.put("yaw", this.getYaw());
+        data.put("pitch", this.getPitch());
+        return data;
+    }
+
+    public static SerializableLocation deserialize(Map<String, Object> args) {
+        String worldName = (String) args.get("world");
+        World world = worldName != null ? Bukkit.getWorld(worldName) : null;
+        return new SerializableLocation(new Location(world, NumberConversions.toDouble(args.get("x")),
+                NumberConversions.toDouble(args.get("y")), NumberConversions.toDouble(args.get("z")),
+                NumberConversions.toFloat(args.get("yaw")), NumberConversions.toFloat(args.get("pitch"))));
+    }
+}
